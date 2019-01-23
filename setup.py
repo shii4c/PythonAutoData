0f5ef7d96e56def0c631f09b5b4b0eb288a744ae
四井 賢一郎 <shii@alkmaar.advanced-media.co.jp>
2019-01-23 14:11:16 +0900

    delete setup.cfg and write all infomation to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
 
-setup()
+setup(
+    name = 'autodata',
+    version = '0.1',
+    url = 'https://github.com/shii4c/PythonAutoData',
+    author = 'Kenichiro SHII',
+    author_email = 'shii4c@gmail.com',
+    license = 'MIT',
+    description = 'Auto allocation dict and list',
+    classifiers = [
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License'
+    ],
+    zip_safe = False,
+    packages = ['autodata']
+)
